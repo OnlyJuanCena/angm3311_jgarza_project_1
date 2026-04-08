6f08c0667ab415144ff74122eb7153ab2d950c8b
JuanMAC <jjgarchomp@gmail.com>
2026-04-07 22:55:15 -0500

    Imported christmas_tree PySide6 window ui code

--- project1.py
+++ project1.py
@@ -1,4 +1,67 @@
 import maya.cmds as cmds
+import maya.OpenMayaUI as omui
+from PySide6 import QtWidgets
+from shiboken6 import wrapInstance
+
+
+def get_maya_main_win():
+    """Return the Maya main window"""
+    main_win_addr = omui.MQtUtil.mainWindow()
+    wrapInstance(int(main_win_addr), QtWidgets.QWidget)
+
+
+class ChristmasTreeWin(QtWidgets.QDialog):
+
+    def __init__(self):
+        super().__init__(parent=get_maya_main_win())
+        self.christmasTree = ChristmasTree()
+        self.setWindowTitle("Christmas Tree Generator")
+        # self.resize(500, 200)
+        self._mk_main_layout()
+        self._connect_signals()
+
+    def build_tree(self):
+        self.christmasTree.trunk_height = self.trunk_height_dspnbx.value()
+        self.christmasTree.canopy_levels = self.canopy_levels_spnbx.value()
+        self.christmasTree.generate_tree()
+
+    def _connect_signals(self):
+        self.build_btn.clicked.connect(self.build_tree)
+        self.cancel_btn.clicked.connect(self.close)
+
+    def _mk_main_layout(self):
+        self.main_layout = QtWidgets.QVBoxLayout()  # Creates our vertical box layout.
+        self._mk_trunk_option_ui()
+        self._mk_canopy_option_ui()
+        self._mk_buttons_layout()
+        self.setLayout(self.main_layout)
+
+    def _mk_canopy_option_ui(self):
+        self.canopy_levels_layout = QtWidgets.QHBoxLayout()
+        self.canopy_levels_lbl = QtWidgets.QLabel("Canopy Levels")
+        self.canopy_levels_spnbx = QtWidgets.QSpinBox()
+        self.canopy_levels_spnbx.setMinimumWidth(50)
+        self.canopy_levels_spnbx.setValue(3)
+        self.canopy_levels_layout.addWidget(self.canopy_levels_lbl)
+        self.canopy_levels_layout.addWidget(self.canopy_levels_spnbx)
+        self.main_layout.addLayout(self.canopy_levels_layout)
+
+    def _mk_trunk_option_ui(self):
+        self.trunk_options_layout = QtWidgets.QHBoxLayout()
+        self.trunk_height_lbl = QtWidgets.QLabel("Trunk Height")
+        self.trunk_height_dspnbx = QtWidgets.QDoubleSpinBox()
+        self.trunk_height_dspnbx.setMinimumWidth(50)
+        self.trunk_height_dspnbx.setValue(1.0)
+        self.trunk_height_dspnbx.setSingleStep(0.1)
+        self.trunk_options_layout.addWidget(self.trunk_height_lbl)
+        self.trunk_options_layout.addWidget(self.trunk_height_dspnbx)
+        self.main_layout.addLayout(self.trunk_options_layout)  # Directs the Dialog Window to use the main layout
+
+    def _mk_buttons_layout(self):
+        self.build_btn = QtWidgets.QPushButton("Build")
+        self.cancel_btn = QtWidgets.QPushButton("Cancel")
+        self.main_layout.addWidget(self.build_btn)  # Adds the build button into one of the rows in the vertical layout
+        self.main_layout.addWidget(self.cancel_btn)  # Adds the build button into one of the rows in the vertical layout
 
 
 class Building():
@@ -15,7 +78,8 @@ class Building():
                                       width=self.building_width,
                                       name="base")[0]
         # move to y=0
-        cmds.xform(building_name, translation=[0, self.building_height / 2.0, 0])
+        cmds.xform(building_name,
+                   translation=[0, self.building_height / 2.0, 0])
         self._freeze_transforms(building_name)
         self._set_pivot_to_origin(building_name)
 
@@ -24,8 +88,8 @@ class Building():
         for level in range(self.building_levels):
             # create cubes
             cube_name = cmds.polyCube(height=self.levels_height,
-                                      depth=self.building_length + 1,
-                                      width=self.building_width + 1,
+                                      depth=self.building_length + 0.5,
+                                      width=self.building_width + 0.5,
                                       name="cube")[0]
             cmds.xform(cube_name, translation=[0, self.levels_height / 2.0, 0])
             self._freeze_transforms(cube_name)
@@ -33,8 +97,7 @@ class Building():
 
             # move cubes into position
             if level != self.building_levels - 1:
-                cmds.xform(cube_name,
-                           translation=[0, self.building_height / (level + 1), 0])
+                cmds.xform(cube_name, translation=[0, self.building_height / (level + 1), 0])
             cube_names.append(cube_name)
 
         # group cubes into group
